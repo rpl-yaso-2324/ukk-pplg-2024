0085f7ca0391d351aa214d581b0e177286678da2
rabbanisan <bani.ramadhan99@gmail.com>
2024-04-23 11:29:42 +0700

    commit

--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,17 @@
-let pin = document.getElementById("pin");
+var pin = document.getElementById("pin");
 user = {
-	pin: 123456,
+	pinUser: 123456,
 };
 cekpin = () => {
-	if (pin.value != user.pin) {
-		alert("pin salah");
-	} else if (pin.value == "") {
+	if (pin.value == "") {
 		alert("pin harus diisi");
+	} else if (pin.value != user.pinUser) {
+		alert("pin salah");
 	} else {
 		window.location.href = "pilihNominal.html";
 	}
 };
-const nominal = {
+const pilihanNominal = {
 	seRatus: "100000",
 	duaRatus: "200000",
 	tigaRatus: "300000",
@@ -20,16 +20,16 @@ const nominal = {
 	satuJutaLimaRatus: "1500000",
 };
 
-let saldo = 1000000;
+var saldo = 1000000;
 const inputNominal = document.getElementById("nominal");
-function pilihNominal(saldoPilihan) {
-	if (saldoPilihan == "" || saldoPilihan == 0) {
+function pilihNominal(nominal) {
+	if (nominal == "" || nominal == 0) {
 		alert(`nominal tidak boleh kosong`);
-	} else if (saldo >= saldoPilihan) {
-		if (saldoPilihan % 50000 == 0) {
+	} else if (saldo >= nominal) {
+		if (nominal % 50000 == 0) {
 			confirm(
-				`anda mengambil uang ${saldoPilihan} dan sisa saldo anda adalah${
-					saldo - saldoPilihan
+				`anda mengambil uang ${nominal} dan sisa saldo anda adalah ${
+					saldo - nominal
 				}`
 			);
 		} else {
